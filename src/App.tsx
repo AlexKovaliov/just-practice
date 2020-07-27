02492ff6eb11908ff0e604434cb015178a75231d
AlexKovaliov <lawsansana@gmail.com>
2020-07-27 14:22:31 +0300

    practice of creating components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    debugger
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+        </div>
+    )
 }
 
-export default App;
+function Star() {
+    return (
+        <div>Star</div>
+    )
+}
+
+function AppTitle() {
+    return (
+        <div>
+            <h1>This is APP component</h1>
+        </div>
+    )
+}
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    return (
+        <div>
+            <h3>Menu</h3>
+        </div>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+function Accordion() {
+    debugger
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
